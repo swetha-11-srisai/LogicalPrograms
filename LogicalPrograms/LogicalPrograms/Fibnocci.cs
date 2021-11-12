2160f84f5e3db20018d18e6b6cb04aa5ca3292a7
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-11 20:38:57 -0800

    [Swetha] add : Fibnoccie series program added

---
+++ LogicalPrograms/LogicalPrograms/Fibnocci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class Fibnocci
+    {
+        int num1 = 0, num2 = 1, nextterm, num;
+        public void FibnocieSeries()
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                nextterm = num1 + num2;
+                num1 = num2;
+                num2 = nextterm;
+                Console.WriteLine("Fibnociee Series :" + nextterm);
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-CoinFlip,2-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Fibnocci series = new Fibnocci();
+                        series.FibnocieSeries();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
