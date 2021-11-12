0c3b50c6e65240cdcc2fbd73c0938d4aea03fc09
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-12 15:56:58 -0800

    [Swetha] add : calculated monthly payment

---
+++ LogicalPrograms/LogicalPrograms/MonthlyPayment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class MonthlyPayment
+    {
+        
+        public  double  monthlyCheck(double p, double y, double r)
+        {
+            
+            double  n = 12 * y;
+            double rone = r / (12 * 100);
+            double payment=p * r / (1 - Math.Pow((1 + rone), -n));
+            return payment;
+        }
+
+
+    }
+}
